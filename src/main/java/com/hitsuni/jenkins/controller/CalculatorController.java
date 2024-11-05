96925ad7366c0842446f89e0097d6e40b4cbedbd
aosskfdlrla <gmltns885@gmail.com>
2024-11-05 11:42:20 +0900

    second

--- src/main/java/com/hitsuni/jenkins/controller/CalculatorController.java
+++ src/main/java/com/hitsuni/jenkins/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm live here";
+        return "I'm live here ha ha ha ha";
     }
 
     @GetMapping("/plus")
